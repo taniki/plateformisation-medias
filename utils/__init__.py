7a936374a88176e4ad79a13f0857a4fdaf228f31
duong tam kien <tk@renard.ninja>
2023-06-28 23:55:28 +0200

    sépare l'affichage des index SIREN

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
+def format_index_siren(style):
+    return (
+        style
+        .format_index(lambda siren: f'<a href="https://annuaire-entreprises.data.gouv.fr/entreprise/{siren}">{siren}</a>')        
+    )
+
 def format_table(df):
     return (
         df
         .style
-        .format_index(lambda siren: f'<a href="https://annuaire-entreprises.data.gouv.fr/entreprise/{siren}">{siren}</a>')
         .format(thousands = " ", precision = 0)
     )
